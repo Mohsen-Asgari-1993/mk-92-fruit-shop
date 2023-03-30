815f14cfa4c090eaa1323b6b7246ed396cfda7be
maxixking66 <maxixking66@gamil.com>
2023-03-30 10:28:09 +0430

    add base entity

---
+++ src/main/java/ir/maktabsharif92/fruitshop/base/domain/Entity.java
@@ -0,0 +1,21 @@
+package ir.maktabsharif92.fruitshop.base.domain;
+
+public abstract class Entity {
+
+    private Long id;
+
+    public Entity() {
+    }
+
+    public Entity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
