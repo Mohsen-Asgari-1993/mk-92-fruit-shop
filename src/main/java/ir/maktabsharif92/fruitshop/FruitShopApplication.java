dc24507d01ef920834e8fc01d66cbdeaca963b6c
maxixking66 <maxixking66@gamil.com>
2023-03-30 16:18:47 +0430

    add example for address repo

--- src/main/java/ir/maktabsharif92/fruitshop/FruitShopApplication.java
+++ src/main/java/ir/maktabsharif92/fruitshop/FruitShopApplication.java
@@ -1,8 +1,20 @@
 package ir.maktabsharif92.fruitshop;
 
+import ir.maktabsharif92.fruitshop.domain.Address;
+import ir.maktabsharif92.fruitshop.util.ApplicationContext;
+
 public class FruitShopApplication {
 
     public static void main(String[] args) {
+
+        Address address = new Address();
+        address.setTitle("عنوان");
+        address.setPostalCode("1234567891");
+        address.setAddress("تهران، آزادی");
+        ApplicationContext.getAddressRepository().save(address);
+
+        Address addressById = (Address) ApplicationContext.getAddressRepository().getById(1L);
+        System.out.println(addressById);
 
     }
 }
--- src/main/java/ir/maktabsharif92/fruitshop/repository/Database.java
+++ src/main/java/ir/maktabsharif92/fruitshop/repository/Database.java
@@ -3,25 +3,25 @@ package ir.maktabsharif92.fruitshop.repository;
 import ir.maktabsharif92.fruitshop.base.domain.User;
 import ir.maktabsharif92.fruitshop.domain.*;
 
-public class Database {
+public abstract class Database {
 
     private static final int DEFAULT_ARRAY_LENGTH = 5;
 
-    private final User[] users = new User[DEFAULT_ARRAY_LENGTH];
+    public static final User[] users = new User[DEFAULT_ARRAY_LENGTH];
 
-    private final Address[] addresses = new Address[DEFAULT_ARRAY_LENGTH];
+    public static final Address[] addresses = new Address[DEFAULT_ARRAY_LENGTH];
 
-    private final Delivery[] deliveries = new Delivery[DEFAULT_ARRAY_LENGTH];
+    public static final Delivery[] deliveries = new Delivery[DEFAULT_ARRAY_LENGTH];
 
-    private final Fruit[] fruits = new Fruit[DEFAULT_ARRAY_LENGTH];
+    public static final Fruit[] fruits = new Fruit[DEFAULT_ARRAY_LENGTH];
 
-    private final Order[] orders = new Order[DEFAULT_ARRAY_LENGTH];
+    public static final Order[] orders = new Order[DEFAULT_ARRAY_LENGTH];
 
-    private final OrderDelivery[] orderDeliveries = new OrderDelivery[DEFAULT_ARRAY_LENGTH];
+    public static final OrderDelivery[] orderDeliveries = new OrderDelivery[DEFAULT_ARRAY_LENGTH];
 
-    private final OrderFruit[] orderFruits = new OrderFruit[DEFAULT_ARRAY_LENGTH];
+    public static final OrderFruit[] orderFruits = new OrderFruit[DEFAULT_ARRAY_LENGTH];
 
-    private final Tag[] tags = new Tag[DEFAULT_ARRAY_LENGTH];
+    public static final Tag[] tags = new Tag[DEFAULT_ARRAY_LENGTH];
 
 
 }
