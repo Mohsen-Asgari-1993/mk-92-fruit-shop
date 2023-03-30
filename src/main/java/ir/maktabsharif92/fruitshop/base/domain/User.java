2def306d80e7015b0ad10054345fafbfbb68b583
maxixking66 <maxixking66@gamil.com>
2023-03-30 10:39:44 +0430

    add users

---
+++ src/main/java/ir/maktabsharif92/fruitshop/base/domain/User.java
@@ -0,0 +1,67 @@
+package ir.maktabsharif92.fruitshop.base.domain;
+
+public class User extends Entity {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    private String nationalCode;
+
+    private String mobileNumber;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+}
---
+++ src/main/java/ir/maktabsharif92/fruitshop/domain/Customer.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif92.fruitshop.domain;
+
+import ir.maktabsharif92.fruitshop.base.domain.User;
+
+public class Customer extends User {
+
+}
